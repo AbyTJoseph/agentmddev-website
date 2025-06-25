f313981ff5feb7bff8d68ea77c07992813eb3baf
Aby Thannikal Joseph <41450362+AbyTJoseph@users.noreply.github.com>
2025-06-25 22:14:21 +0100

    fix: add static output and valid package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://agentmd.dev',
+  output: 'static',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
